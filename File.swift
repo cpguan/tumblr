962d04b4bcf946628779d35f8913b3b8cc5cf9dc
Pan Guan <Pan@Chupengs-MacBook-Pro.local>
2017-02-02 23:11:16 -0500

    first commit

---
+++ File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pan Guan on 2/2/17.
+//
+//
+
+import Foundation
---
+++ Tumblr/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by Pan Guan on 2/2/17.
+//  Copyright © 2017 Pan. All rights reserved.
+//
+
+import UIKit
+class PhotoCell: UITableViewCell {
+    
+    @IBOutlet weak var PostView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+}
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
